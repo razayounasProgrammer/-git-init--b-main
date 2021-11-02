b5f859583b90a4c9079fc357e939705c480bd67c
Muhammad Raza Younas <93576264+razayounasProgrammer@users.noreply.github.com>
2021-11-02 13:13:57 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+
+    <section class="form">
+        <div class="container section">
+            <div class="row">
+                <div class="col-md-4 section-one">
+                    <i class='bx bxs-ghost icon'></i>
+                    <div class="not">
+                        <h2 class="note">Welcome Back!</h2>
+                        <p class="description">To Keep connected with us please login with your persoanl info</p>
+                    </div>
+                    <div class="button">
+                        <button type="submit" class="main-btn">SIGN IN</button>
+                    </div>
+                </div>
+                <div class="col-md-8 section-two">
+                    <h2 class="title">Create Account</h2>
+                    
+                    <!-- Social Icon Links -->
+                    <div class="social-icon">
+                        <ul class="links d-flex justify-content-center mt-2">
+                            <li class="nav-item"><i class='bx bxl-facebook-circle'></i></li>
+                            <li class="nav-item"><i class='bx bxl-google-plus'></i></li>
+                            <li class="nav-item"><i class='bx bxl-linkedin'></i></li>
+                        </ul>
+                    </div>
+                    <p class="description">or user your email for registration</p>
+                    <form>
+                        <div class="form-group mb-3">
+                            <input type="text" class="form-control field" placeholder="User Name">
+                        </div>
+                        <div class="form-group mb-3">
+                            <input type="email" class="form-control field" placeholder="Email">
+                        </div>
+                        <div class="form-group mb-3">
+                            <input type="password" class="form-control field" placeholder="Password">
+                        </div>
+                        <div class="button-two">
+                            <button type="submit" class="main-btn-login">SIGN UP</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
